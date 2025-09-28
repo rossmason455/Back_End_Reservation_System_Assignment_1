042fee661c9c5287f222edb3ba4218080f317701
Ross Mason <N00230645@iadt.ie>
2025-09-28 21:23:17 +0100

    created mongodb models folder, created a routes folder & added a server.js file

--- mongodb_models/index.js
+++ mongodb_models/index.js
@@ -1,17 +1,3 @@
-const express = require ('express')
-const mongoose = require ('mongoose')
-const app = express()
-
-
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
-
-app.get('/',(req,res) => {
-res.send('Hello from Node API')
-})
-
 mongoose.connect("mongodb+srv://n00230645_db_user:cHH09IOEsM7mTs3l@reservationmongodb.ef34u6s.mongodb.net/reservation_mongodb?retryWrites=true&w=majority&appName=reservationmongodb")
   .then(() => {
         console.log("connected to database")
