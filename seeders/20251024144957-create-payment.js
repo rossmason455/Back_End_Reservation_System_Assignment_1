78af822dd019f99447b9514e1df6211aa694f7ff
Ross Mason <N00230645@iadt.ie>
2025-10-24 15:52:42 +0100

    created payment seeder to generate and assign payments each booking

---
+++ seeders/20251024144957-create-payment.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   const bookings = await queryInterface.sequelize.query(
+      'SELECT id FROM bookings;',
+      { type: Sequelize.QueryTypes.SELECT }
+    );
+
+    const payments = [];
+
+    for (const booking of bookings) {
+      
+      if (faker.datatype.boolean()) {
+        payments.push({
+          booking_id: booking.id,
+          amount: faker.finance.amount({ min: 20, max: 500, dec: 2 }),
+          status: faker.helpers.arrayElement(['pending', 'completed', 'failed']),
+          payment_date: new Date(),
+        });
+      }
+    }
+
+    await queryInterface.bulkInsert('payments', payments, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+   await queryInterface.bulkDelete('payments', null, {});
+  }
+};
