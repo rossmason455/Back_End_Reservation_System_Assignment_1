9414b7b0fb13d19d7b757808853743ea1b1c7416
Ross Mason <N00230645@iadt.ie>
2025-10-24 15:11:40 +0100

    created resource seeder to handle inserting doctors, restaurants and meeting rooms into resources table

---
+++ seeders/20251024133153-create-resources.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+    for (let i = 0; i < 20; i++) {
+      resources.push({
+        name: `Dr. ${faker.name.lastName()}`,
+        type: 'doctor',
+        status: 'available',
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+    }
+
+    
+    for (let i = 0; i < 20; i++) {
+      const restaurantName = `${faker.company.bsAdjective()} ${faker.company.companyName()}`;
+      resources.push({
+        name: restaurantName,
+        type: 'restaurant',
+        status: 'available',
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+    }
+
+    
+    for (let i = 0; i < 20; i++) {
+      const roomName = `Meeting Room ${faker.word.adjective()} ${faker.word.noun()}`;
+      resources.push({
+        name: roomName,
+        type: 'meeting_room',
+        status: 'available',
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('resources', null, {});
+  }
+};
