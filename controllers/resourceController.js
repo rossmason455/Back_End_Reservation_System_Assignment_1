c36e29de6c7f3a7bce4d3ed5cb2addadba6a9034
Ross Mason <N00230645@iadt.ie>
2025-10-25 20:29:05 +0100

    added logic for retrieving mongodb resource detail as well sql resource in rest client response, switch case checks the resource type and find the adjacent mongodb resource id

--- controllers/resourceController.js
+++ controllers/resourceController.js
@@ -1,4 +1,7 @@
 const { Resource } = require("../models");
+const DoctorDetail = require("../mongodb_models/doctorDetail");
+const RestaurantDetail = require("../mongodb_models/restaurantDetail");
+const MeetingRoomDetail = require("../mongodb_models/meetingRoomDetail");
 
 exports.getResourceById = async (req, res) => {
   try {
@@ -10,11 +13,34 @@ exports.getResourceById = async (req, res) => {
       return res.status(404).json({ message: "Resource not found" });
     }
 
-    
+     let detail = null;
 
+    switch (resource.type) {
+      case "doctor":
+        detail = await DoctorDetail.findOne({ my_sql_resource_id: id });
+        break;
 
+      case "restaurant table":
+        detail = await RestaurantDetail.findOne({ my_sql_resource_id: id });
+        break;
 
-    return res.status(200).json(resource);
+      case "meeting room":
+        detail = await MeetingRoomDetail.findOne({ my_sql_resource_id: id });
+        break;
+
+      default:
+        break;
+    }
+
+     const response = {
+      resource, 
+      detail    
+    };
+
+    return res.status(200).json({
+      message: "Resource retrieved successfully",
+      data: response,
+    });
   } catch (error) {
     console.error("Error fetching resource:", error);
     return res.status(500).json({ message: "Server error" });
