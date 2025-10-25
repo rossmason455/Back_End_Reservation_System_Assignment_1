e3d6a5d09f253b1bd992045053e0d8bf529e26dd
Ross Mason <N00230645@iadt.ie>
2025-10-25 23:03:47 +0100

    created controller and route files for payments

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const app = express();
 const auth = require('./routes/auth');
 const resourceRoutes = require('./routes/resourceRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
 require('./mongodb_models/index');
 
 app.use(express.json());
 app.use('/api/auth', auth);
 app.use('/api/resource', resourceRoutes);
 app.use('/api/booking', bookingRoutes);
+app.use('/api/payment', paymentRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello from Node API');
