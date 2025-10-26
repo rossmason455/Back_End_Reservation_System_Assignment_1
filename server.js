0d2f2b9faf556833654dee75f4b9a8bccbaac792
Ross Mason <N00230645@iadt.ie>
2025-10-26 16:28:10 +0000

    seperated the app definiton file (server.js) from starting the server so supertest can test the instance of the actaul app not the app running on the server, also added logic to tests/setup for connecting and closing the connection to mongodb test db when running tests, and updated package.json to tell jest to run setup.js first before the other tests files

--- server.js
+++ server.js
@@ -17,8 +17,4 @@ app.get('/', (req, res) => {
     res.send('Hello from Node API');
 });
 
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(3000, () => {
-    console.log('Server running on port 3000');
-  });
-}
+module.exports = app;
--- tests/setup.js
+++ tests/setup.js
@@ -1,9 +1,18 @@
 const { sequelize } = require("../models");
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 beforeAll(async () => {
   await sequelize.sync({ force: true });
+
+await mongoose.connect(process.env.MONGO_URL_TEST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 });
 
 afterAll(async () => {
   await sequelize.close();
+
+  await mongoose.connection.close();
 });
